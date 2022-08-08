5f456a5552195e7e2ce5f4c87dbc603bf844d701
PetroGal <petergalp@gmail.com>
2022-08-08 21:49:20 +0300

    first commit with navbar and header

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-/*
-================================================================== 
-                           SELECT SPAN
-==================================================================
-*/
-const date = (document.getElementById('date').innerHTML =
-  new Date().getFullYear())
-
-/*
-================================================================== 
-                           NAV TOGGLE
-==================================================================
-*/
-// ****************** SELECT BUTTON AND LINKS ********************
-const navBtn = document.getElementById('nav-toggle')
-const links = document.getElementById('nav-links')
-// add event listener
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links')
+const navbar = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+const closeBtn = document.querySelector('#close-btn')
+const sidebar = document.querySelector('#sidebar')
+const date = document.querySelector('#date')
+// add fixed class to navbar
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed')
+  } else {
+    navbar.classList.remove('navbar-fixed')
+  }
 })
-
-// ******************* SMOOTH SCROLL *****************************
-// select links
-const scrollLinks = document.querySelectorAll('.scroll-link')
-scrollLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    // prevent default
-    e.preventDefault()
-    links.classList.remove('show-links')
-
-    const id = e.target.getAttribute('href').slice(1)
-    const element = document.getElementById(id)
-    //
-    let position = element.offsetTop - 62
-
-    window.scrollTo({
-      left: 0,
-      // top: element.offsetTop,
-      top: position,
-      behavior: 'smooth',
-    })
-  })
+// show sidebar
+navBtn.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar')
 })
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar')
+})
+// set year
+date.innerHTML = new Date().getFullYear()
